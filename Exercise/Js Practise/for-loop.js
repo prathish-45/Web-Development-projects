91ddd1e2612730894a0ff2baa143bce38fe026a0
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-06-15 21:06:44 +0530

    15/06/2025 Js Assignments

---
+++ Exercise/Js Practise/for-loop.js
@@ -0,0 +1,10 @@
+
+// Assignment 2
+for(var i = 0; i < 10; i++) {  
+    console.log(i);
+}
+console.log("Loop finished!");
+
+for(var i = 1; i < 10; i += 2) {  
+    console.log(i);
+}
---
+++ Exercise/Js Practise/if-condition.js
@@ -0,0 +1,15 @@
+// Assignment 1
+
+var rain = "yes";
+if (rain === "yes") {
+    console.log("Take an Umbrella!");
+}else {
+    console.log("Enjoy the sunshine.");
+}
+
+var brush = "yes";
+if (brush === "yes") {
+    console.log("Your teeth are clean!");
+}else {
+    console.log("Go brush your teeth.");
+}
