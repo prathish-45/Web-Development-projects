546fe7d538c01409b7c579ae175cc6bd4a69942d
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-06-16 22:09:14 +0530

    16/05/2025 rest.js - Added a new function to test rest functionality

--- Exercise/Js Practise/rest.js
+++ Exercise/Js Practise/rest.js
@@ -5,3 +5,10 @@ const [first, ...rest] = nums;
 console.log(first);
 console.log(rest);
 
+// Function Parameters
+
+function sum(...nums){
+    return nums.reduce((a, b) => (a + b), 0);
+}
+
+console.log(sum(1, 2, 3));
