d39ec1ca37a7dadc0e44cb33c90aaa259e4ab0a2
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-06-16 21:41:16 +0530

    16/06/2025 Rest and Spread Operators in JavaScript

---
+++ Exercise/Js Practise/rest.js
@@ -0,0 +1,7 @@
+// Array
+
+const nums = [10, 20, 30, 40, 50]
+const [first, ...rest] = nums;
+console.log(first);
+console.log(rest);
+
---
+++ Exercise/Js Practise/spread.js
@@ -0,0 +1,13 @@
+// Combine the two array
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+const merged = [...num1, ...num2];
+console.log(merged);
+
+
+// Combine the two object
+const obj1 = {name: "Bob"};
+const obj2 = {age: 23};
+const objMerge = {...obj1, ...obj2};
+console.log(objMerge);
+
