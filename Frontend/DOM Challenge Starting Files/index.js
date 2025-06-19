a5a55bd989466058c32473d525abd87e451f3c51
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-06-19 21:18:57 +0530

    19/05/2024 Update DOM Challenge file (updating index.js)

--- Frontend/DOM Challenge Starting Files/index.js
+++ Frontend/DOM Challenge Starting Files/index.js
@@ -1,2 +1,10 @@
-var test = document.firstElementChild;
-console.log(test);
+var button = document.querySelector("button");
+
+button.addEventListener("click", (event) => {
+    document.querySelector("h1").classList.toggle("huge");
+});
+
+var heading = document.querySelector("h1");
+heading.addEventListener("mouseover", (event) => {
+    heading.classList.toggle("huge");
+})
