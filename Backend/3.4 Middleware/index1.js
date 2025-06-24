aa55ea7349fe9836b5ade084142fa08452c8e143
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-06-24 22:29:35 +0530

    24/06/2025 Middleware Learning

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -1,16 +1,24 @@
-import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import bodyParser from 'body-parser';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log(__dirname);
   res.sendFile(__dirname + "/public/index.html");
-});
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.sendFile(__dirname + "/public/submit.html");
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
- 
+  console.log(`Server running in ${port}`);
+})
--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log("Request Method: " + req.method);
+  console.log("Request URL: " + req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
