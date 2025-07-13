d96d3c5e42fecf7635cc0b242d3dae0d8a1d4c04
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-07-13 22:16:49 +0530

    13/07/2025 React components

---
+++ React/286-react-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./components/Heading";
+import List from "./components/List";
+
+function App() {
+    return (
+        <div>
+            <Heading />
+            <List />
+        </div>
+        
+    )
+}
+
+export default App;
