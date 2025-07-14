eea3bb70214200b263aac00808bb4be54feb509b
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-07-14 20:10:30 +0530

    14/07/2025 React Components practice

---
+++ React/287-react-components-practice/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./components/Heading";
+
+function App() {
+    return (
+        <div>
+            <Heading />
+        </div>
+    )
+}
+
+export default App;
