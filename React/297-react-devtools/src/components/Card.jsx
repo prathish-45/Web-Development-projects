d9e08738d147dd3d33db93195a03d46940296882
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-07-16 20:54:55 +0530

    16/07/2025 React Devtools

--- React/297-react-devtools/src/components/Card.jsx
+++ React/297-react-devtools/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "./Avatar";
+import Contact from "./Contact";
 
 function Card(props) {
   return (
@@ -9,8 +10,8 @@ function Card(props) {
         <Avatar img = {props.img}/>
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <Contact detailInfo = {props.tel}/>
+        <Contact detailInfo = {props.email} />
       </div>
     </div>
   );
