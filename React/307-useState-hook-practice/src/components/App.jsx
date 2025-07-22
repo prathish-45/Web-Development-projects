b2d82995a72e1625d96a2591a6bfaac25b31c100
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-07-22 22:03:57 +0530

    22/07/2025 useState hook practice

--- React/307-useState-hook-practice/src/components/App.jsx
+++ React/307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+
+
+let time = new Date().toLocaleTimeString('en-US', { hour12: false });
 
 function App() {
+
+  const [count, setCount] = useState(time);
+
+  function getTime() {
+    let currTime = new Date().toLocaleTimeString('en-US', { hour12: false });
+    setCount(currTime);
+  }
+
+  setInterval(getTime, 1000)
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{count}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
--- React/307-useState-hook-practice/src/index.jsx
+++ React/307-useState-hook-practice/src/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);
 
 //Challenge:
 //1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
+// let time = new Date().toLocaleTimeString();
+// console.log(time);
 //Show the latest time in the <h1> when the Get Time button
 //is pressed.
 
@@ -16,9 +16,11 @@ console.log(time);
 //Can you get the time in your <h1> to update every second?
 
 //e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
+
+function sayHi() {
+  console.log("Hey");
+}
+setInterval(sayHi, 1000)
 // setInterval(sayHi, 1000);
 
 // If you're running this locally in VS Code use the commands:
