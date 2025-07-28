242ed60c9462b1ecccf3ae52aaf8c71fb1ccb04b
prathish-45 <prathishsit2427@ksrce.ac.in>
2025-07-28 20:05:32 +0530

    28/07/2025 Managing a component tree

---
+++ React/317-managing-a-component-tree/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM, {createRoot} from "react-dom/client";
+import App from "./components/App";
+
+createRoot(document.getElementById("root")).render(<App />);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
